20f9ffac5558fa4bb554d4f6918d927db218da51
Andreas Josefsson <andreas@barnensbibliotek.se>
2018-04-09 15:03:26 +0200

    2018-04-09 RELEASE version!

--- kulturkatalogenUtovare/Dal/utovareDAL.vb
+++ kulturkatalogenUtovare/Dal/utovareDAL.vb
@@ -1,6 +1,8 @@
 ﻿Public Class utovareDAL
+    'Private _connectionString As String = "Data Source=.\SQLEXPRESS;Initial Catalog=kulturkatalogenDB_v1;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
+    Private _connectionString As String = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB_DEV;User ID=kulturkatalogenDB;Password=L0rda1f"
     'Private _connectionString As String = "Data Source=.\SQLEXPRESS;Initial Catalog=dnndev_v902.me;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
-    Private _connectionString As String = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB;User ID=kulturkatalogenDB;Password=L0rda1f"
+    'Private _connectionString As String = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB;User ID=kulturkatalogenDB;Password=L0rda1f"
     Private _linqObj As New utovareLinqDataContext(_connectionString)
 
 
