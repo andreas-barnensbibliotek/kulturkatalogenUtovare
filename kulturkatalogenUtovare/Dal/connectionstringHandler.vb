8528b77191771ed240db51d462f42b04ce18303f
Andreas Josefsson <andreas@barnensbibliotek.se>
2018-07-04 14:16:41 +0200

    2018-07-04 uppdaterat innan semestern

---
+++ kulturkatalogenUtovare/Dal/connectionstringHandler.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+Imports System.Configuration
+Imports System.Globalization
+Imports System.ComponentModel
+Imports System.Collections.Specialized
+
+
+Public Class connectionstringHandler
+    Private _connectionstring As String
+
+    Public Sub New()
+        ' _connectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=kulturkatalogenDB_v1;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
+        ' _connectionString = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB_DEV;User ID=kulturkatalogenDB;Password=L0rda1f"
+        '_connectionstring = "Data Source=.\SQLEXPRESS;Initial Catalog=dnndev_v902.me;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
+        ' _connectionString = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB;User ID=kulturkatalogenDB;Password=L0rda1f"
+        '_connectionstring = "Data Source=DE-2697;Initial Catalog=kulturkatalogenDB_DEV;User ID=KulturkatalogenDB_DEV;Password=L0rda1f"
+        'SKARP NYA SERVERN
+        _connectionstring = "Data Source=DE-2697;Initial Catalog=kulturkatalogenDB;User ID=KulturkatalogenDB2;Password=L0rda1f"
+    End Sub
+    Public ReadOnly Property CurrentConnectionString() As String
+        Get
+            Return _connectionstring
+        End Get
+
+    End Property
+
+
+End Class
--- kulturkatalogenUtovare/Dal/utovareDAL.vb
+++ kulturkatalogenUtovare/Dal/utovareDAL.vb
@@ -1,10 +1,6 @@
 ﻿Public Class utovareDAL
-    'Private _connectionString As String = "Data Source=.\SQLEXPRESS;Initial Catalog=kulturkatalogenDB_v1;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
-    Private _connectionString As String = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB_DEV;User ID=kulturkatalogenDB;Password=L0rda1f"
-    'Private _connectionString As String = "Data Source=.\SQLEXPRESS;Initial Catalog=dnndev_v902.me;Persist Security Info=True;User ID=dnndev_v902.me;Password=L0rda1f"
-    'Private _connectionString As String = "Data Source=DE-1896;Initial Catalog=kulturkatalogenDB;User ID=kulturkatalogenDB;Password=L0rda1f"
-    Private _linqObj As New utovareLinqDataContext(_connectionString)
-
+    Dim connstring As New connectionstringHandler
+    Private _linqObj As New utovareLinqDataContext(connstring.CurrentConnectionString)
 
     Public Function getUsername(name As String) As List(Of utovareDetailInfo)
         Dim ret As New List(Of utovareDetailInfo)
